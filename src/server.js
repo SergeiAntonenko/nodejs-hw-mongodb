cf877bbbea211795b760aa8496cae06ecc9f39cf
SergeiAntonenko <gerbar50@gmail.com>
2024-06-03 21:51:43 +0300

    upd hw

---
+++ src/server.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+dotenv.config();
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+      res.status(200).json({
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (e) {
+      res.status(500).json({
+        message: 'Ups something went wrong...',
+      });
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    try {
+      const contact = await getContactById(contactId);
+      res.status(200).json({
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (e) {
+      res.status(404).json({
+        message: `Contact with id ${contactId} not found!`,
+      });
+    }
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(3000, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
+};

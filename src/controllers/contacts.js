f9eaf3b759b152a536e04f33089953ad692f53eb
SergeiAntonenko <gerbar50@gmail.com>
2024-06-11 23:38:04 +0300

    upd hw 2

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -12,6 +12,7 @@ export const getContactsController = async (req, res) => {
   const contacts = await getAllContacts();
 
   res.status(200).json({
+    status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
   });
@@ -32,6 +33,7 @@ export const getContactByIdController = async (req, res, next) => {
   }
 
   res.status(200).json({
+    status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
   });
@@ -69,7 +71,7 @@ export const patchContactController = async (req, res, next) => {
   });
 };
 
-export const deleteContactController = async (req, res) => {
+export const deleteContactController = async (req, res, next) => {
   const { contactId } = req.params;
   if (!mongoose.Types.ObjectId.isValid(contactId)) {
     next(createHttpError(404, 'Contact not found'));

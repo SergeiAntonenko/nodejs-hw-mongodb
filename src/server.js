7bdc366c67257f790fc5a2c9344e0432bf5f567b
SergeiAntonenko <gerbar50@gmail.com>
2024-07-12 15:11:31 +0300

    hw 7

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { env } from './utils/env.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
@@ -21,6 +22,8 @@ export const setupServer = () => {
       },
     }),
   );
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
